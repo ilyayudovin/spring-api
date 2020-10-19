0875d9f6314e2291400b1bd1fab36642f8808910
ilyayudovin <ilyayudovin123@gmail.com>
2020-10-19 13:14:08 +0300

    Delete cookie package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import loginRoutes from './routes/login.js';
 import projectsRoutes from './routes/projects.js';
@@ -9,7 +8,6 @@ const app = express();
 const PORT = 3030;
 
 app.use(cors());
-app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use('/login', loginRoutes);
