6063d455f1a68e525adf7c7c7ad31b0070c90d2a
ilyayudovin <ilyayudovin123@gmail.com>
2020-10-23 18:12:44 +0300

    Add signup route

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import registerRoutes from './routes/signup.js';
 import loginRoutes from './routes/login.js';
 import projectsRoutes from './routes/projects.js';
-
 const app = express();
-const PORT = 3030;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use('/signup', registerRoutes);
 app.use('/login', loginRoutes);
 app.use('/projects', projectsRoutes);
 
