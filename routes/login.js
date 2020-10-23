9f8448daa7f52c1c00b897e1202cf97a999bdcf5
ilyayudovin <ilyayudovin123@gmail.com>
2020-10-23 18:16:15 +0300

    Make proper login

--- routes/login.js
+++ routes/login.js
@@ -1,21 +1,47 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import asyncHandler from './../middlewares/asyncMiddleware.js';
+import {generateToken} from "../jwt/token.js";
+import { pool } from './../dbConfig.js';
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
-const generateToken = (user) => {
-  const signature = 'my-super-secret';
-  const expiration = '6h';
-  return jwt.sign({ user }, signature, { expiresIn: expiration });
-};
-
 router.post('/', asyncHandler((req, res) => {
-  const user = req.body;
-  const token = generateToken(user);
-  res.json({
-    token: token
-  });
+  const {username, password} = req.body;
+  pool.query('SELECT * FROM public."Users" WHERE username = $1',
+    [username],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      if(result.rows.length > 0){
+        const user = result.rows[0];
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if(err) {
+            throw err;
+          }
+          if(isMatch){
+            const token = generateToken(user);
+            res.json({
+              token: token
+            });
+          }
+          else {
+            res.json({
+              type: 'password',
+              error: 'Incorrect password'
+            })
+          }
+        })
+      }
+      else {
+        res.json({
+          type: 'username',
+          error: 'No such username'
+        });
+      }
+    });
+
 }));
 
 
