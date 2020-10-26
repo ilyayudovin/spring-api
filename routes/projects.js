deae74119d5b3730d17c58e061f27c4e658aa6a9
ilyayudovin <ilyayudovin123@gmail.com>
2020-10-26 14:11:30 +0300

    Fix formatting

--- routes/projects.js
+++ routes/projects.js
@@ -13,12 +13,7 @@ router.get('/', (req, res) => {
 
 router.get('/search', (req, res) => {
   const term = req.query.q.toLowerCase();
-  Project.findAll({
-      where:
-        { name:
-            { [Op.iLike]: `%${term}%` }
-        }
-    })
+  Project.findAll({ where: { name: { [Op.iLike]: `%${term}%` }}})
     .then(projects => res.status(200).json(projects))
     .catch(err => res.status(400).json(err))
 });
